cc3e3a9995c9264a0472f5f347fa56035167ea48
Eleanor Wright <elliewright055@gmail.com>
2025-04-28 14:32:13 -0500

    Add player and blit to screen

--- main.py
+++ main.py
@@ -6,8 +6,18 @@ FPS = 60
 WIDTH = 800
 HEIGHT = 600
 
+PLAYER_ICON = pygame.transform.scale_by(pygame.image.load("woman.png"), 0.1)
+PLAYER_WIDTH = PLAYER_ICON.get_size()[0]
+PLAYER_HEIGHT = PLAYER_ICON.get_size()[1]
+
+player_x = (WIDTH / 2) - (PLAYER_WIDTH / 2)
+player_y = HEIGHT - (PLAYER_HEIGHT * 1.5)
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
+
+def draw_player(x, y):
+    screen.blit(PLAYER_ICON, (x, y))
 
 running = True
 
@@ -17,6 +27,8 @@ while running:
             running = False
     
     screen.fill("thistle1")
+
+    draw_player(player_x, player_y)
 
     pygame.display.flip()
 
